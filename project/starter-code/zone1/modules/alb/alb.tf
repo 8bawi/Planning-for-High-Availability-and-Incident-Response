1917eb274cecc7b99237eff5aa56874528a86c43
8bawi <ahmed.8arabawi@gmail.com>
2023-03-05 14:27:39 +0200

    modifying alb module

--- project/starter-code/zone1/modules/alb/alb.tf
+++ project/starter-code/zone1/modules/alb/alb.tf
@@ -7,7 +7,7 @@ resource "aws_lb_target_group" "this" {
 
 resource "aws_lb_target_group_attachment" "this" {
   count            = var.instance_count
-  target_group_arn = aws_lb_target_group.udacity.arn
+  target_group_arn = aws_lb_target_group.this.arn
   target_id        = var.ec2.*.id[count.index]
   port             = 80
 }
@@ -24,12 +24,12 @@ resource "aws_lb" "this" {
 }
 
 resource "aws_lb_listener" "this" {
-  load_balancer_arn = aws_lb.udacity.arn
+  load_balancer_arn = aws_lb.this.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.udacity.arn
+    target_group_arn = aws_lb_target_group.this.arn
   }
 }
