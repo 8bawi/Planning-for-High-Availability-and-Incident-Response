72e8b778fd8d1699561a5d94f31305338b303bd2
8bawi <ahmed.8arabawi@gmail.com>
2023-03-18 15:40:43 +0200

    sql_push

--- project/starter-code/zone2/_output.tf
+++ project/starter-code/zone2/_output.tf
@@ -9,3 +9,9 @@ output "account_id" {
  output "caller_user" {
    value = data.aws_caller_identity.current.user_id
  }
+  output "vpc_west" {
+   value = data.terraform_remote_state.vpc.outputs.vpc_id
+ }
+  output "subnet_west" {
+   value = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+ }
